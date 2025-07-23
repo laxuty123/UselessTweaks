13b92eb62873103951727e8e55d387fbe5e0afff
laxuty123 <laxuty@proton.me>
2025-07-23 04:17:18 +0200

    feat: update settings.gradle.kts
    Adds fabric loom support

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,12 @@
-rootProject.name = "UselessTweaks"
+pluginManagement {
+    plugins {
+        id("fabric-loom") version(settings.extra["loom_version"] as String)
+    }
+    repositories {
+        maven("https://maven.fabricmc.net/")
+        mavenLocal()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+rootProject.name = settings.extra["mod_name"].toString()
