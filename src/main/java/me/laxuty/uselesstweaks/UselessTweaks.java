2ff5f5d26cca4f0eaac01b321d26772e0826022f
laxuty123 <laxuty@proton.me>
2025-07-23 04:19:52 +0200

    feat: fabric mod implementation
    Adds the nessacary files and source code to run fabric

---
+++ src/main/java/me/laxuty/uselesstweaks/UselessTweaks.java
@@ -0,0 +1,11 @@
+package me.laxuty.uselesstweaks;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public final class UselessTweaks implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
